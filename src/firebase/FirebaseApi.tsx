e0f8c38b737fb07cd773d9e4264a70c41e284977
Vishnu Sundaresan <vishnu.sundaresan@gmail.com>
2022-07-10 02:32:38 +0800

    Auth

--- src/firebase/FirebaseApi.tsx
+++ src/firebase/FirebaseApi.tsx
@@ -13,10 +13,11 @@ import {
   runTransaction,
 } from "firebase/firestore";
 // Authentication
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, UserCredential } from "firebase/auth";
 // API Key
 import { firebaseConfig } from "../../Constants";
 import { UserInStore, FoodItem, UserFoodItem, userInStoreConverter, userFoodItemConverter } from "../Interfaces";
+import { FOODS_COLLECTION_ID, USERS_COLLECTION_ID } from "../AppConstants";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -29,10 +30,13 @@ const db = getFirestore(app);
 const foodsRef = (uid: string) => doc(db, FOODS_COLLECTION_ID, uid).withConverter(userFoodItemConverter);
 
 // Reference to Users Collection
-const usersRef = doc(db, USERS_COLLECTION_ID).withConverter(userInStoreConverter)
+const usersRef = (uid: string) => doc(db, USERS_COLLECTION_ID, uid).withConverter(userInStoreConverter)
 
 // Collections need a doc inside them always
 // Get uid from auth
+
+//-------------------Firebase Auth References-------------------
+const auth = getAuth()
 
 //-------------------Firestore Foods Methods-------------------
 
@@ -44,34 +48,20 @@ const usersRef = doc(db, USERS_COLLECTION_ID).withConverter(userInStoreConverter
  * @returns A promise containing a boolean that returns true if no errors occurred in the process.
  */
 async function addAuthenticatedUserAfterSignup(
-  uid: string,
   user: UserInStore
-): Promise<boolean> {
+) {
   const { name, email } = user;
 
   // Add to users and create food items table
-  const responses = await Promise.allSettled([
-    addDoc(collection(db, USERS_COLLECTION_ID, uid), {
+  await addDoc(collection(db, USERS_COLLECTION_ID), {
       name: name,
-      uid: uid,
       email: email,
-    }),
-    addDoc(collection(db, FOODS_COLLECTION_ID, uid), {
+    })
+  await addDoc(collection(db, FOODS_COLLECTION_ID), {
       householdName: `${name}'s House`,
       foodItems: [], // Should be of type FoodItem
-    }),
-  ]);
-
-  // Log Errors if any
-  responses.forEach((resp) => {
-    if (resp.status === "rejected") {
-      console.error(resp.reason);
-      return false
-    }
-  });
-
-  return true
-}
+    })
+  }
 
 /**
  * Returns a Promise containing the UserInStore data.
@@ -79,8 +69,8 @@ async function addAuthenticatedUserAfterSignup(
  * @param uid User's unique ID from auth.
  * @returns Promise containing UserInStore for corresponding uid.
  */
-async function getUserFromStore(uid: string): Promise<DocumentData | void | undefined> {
-  return getDoc(doc(db, USERS_COLLECTION_ID, uid))
+async function getUserFromStore(uid: string): Promise<UserInStore | void | undefined> {
+  return getDoc(usersRef(uid))
     .then((doc) => doc.data())
     .catch(console.error);
 }
@@ -94,9 +84,9 @@ async function getUserFromStore(uid: string): Promise<DocumentData | void | unde
  */
 async function getFoodItemsByUser(
   uid: string,
-  onNext: (userFoodItem?: DocumentData) => void
+  onNext: (userFoodItem?: UserFoodItem) => void
 ): Promise<Unsubscribe> {
-  const unsubFunc = onSnapshot(doc(db, FOODS_COLLECTION_ID, uid), (doc) => {
+  const unsubFunc = onSnapshot(foodsRef(uid), (doc) => {
     onNext(doc.data());
   });
   return unsubFunc;
@@ -130,4 +120,35 @@ async function deleteFoodItemFromUser (uid: string, foodItem: FoodItem) {
   })
   .then((_resp) => {})
   .catch(console.error)
+}
+
+//-------------------Firestore Auth Methods-------------------
+/**
+ * Logs the user in.
+ * 
+ * @param email User's email.
+ * @param password User's password.
+ * @param success_callback Callback if successful login
+ */
+export const login = async(email: string, password: string, success_callback: (userCred: UserCredential) => void) => {
+  await signInWithEmailAndPassword(auth, email, password)
+    .then(success_callback)
+}
+
+/**
+ * Registers new user
+ * @param name 
+ * @param email 
+ * @param password 
+ * @param success Callback if successful sign-up. 
+ */
+export const signUp = async (name: string, email: string, password: string, success?: (user: UserCredential) => void) => {
+  await createUserWithEmailAndPassword(auth, email, password)
+  .then(async (user: UserCredential) => {
+    await addAuthenticatedUserAfterSignup(user.user.uid, {
+      email: email,
+      name: name,
+    })
+  .then((user) => success?.(user))
+  })
 }
