e8052836806aa8dc9a9258f66200a59848e75ea4
AFFINITYWIN-110\Vishal Jeyaram <vishal.j@affinidi.com>
2022-07-10 02:03:44 +0800

    Home screen added

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import { StyleSheet, StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomePage from './src/pages/HomePage';
+import HomeScreen from './src/pages/HomePage';
 import FoodInput from './src/pages/FoodInput';
 import Signup from './src/pages/Signup';
 import Login from './src/pages/Login';
+
+
 
 type RootStackParamList = {
   Home: { userId: string };
@@ -23,7 +25,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Signup" component={Signup}/>
-        <Stack.Screen name="Home" component={HomePage}/>
+        <Stack.Screen name="Home" component={HomeScreen}/>
         <Stack.Screen name="Foodinput" component={FoodInput}/>
       </Stack.Navigator>
     </NavigationContainer>
