e4f7fb75d6595644c132097dba1fdd21d5f243ac
Mrinal Ganesh <mrinalganesh@gmail.com>
2022-07-10 10:53:31 +0800

    papa anakin rescue puppy

--- src/pages/FoodInput.tsx
+++ src/pages/FoodInput.tsx
@@ -1,142 +1,176 @@
-import React, {useState, useRef, useEffect} from 'react';
-import { Alert, StyleSheet, Text, View, Button, TextInput, SafeAreaView, Platform, Modal, ImageBackground} from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { Camera } from 'expo-camera';
-import { ShareAsync } from 'expo-sharing';
-import * as MediaLibrary from 'expo-media-library'
-import { StatusBar } from 'expo-status-bar';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  SafeAreaView,
+  Platform,
+  Modal,
+  ImageBackground,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Camera } from "expo-camera";
+import { ShareAsync } from "expo-sharing";
+import * as MediaLibrary from "expo-media-library";
+import { StatusBar } from "expo-status-bar";
+import * as ImagePicker from "expo-image-picker";
+import { uploadImage } from "../firebase/FirebaseApi";
+import { addFoodItemToUser } from "../firebase/FirebaseApi";
+import { getCurrentUserUid } from "../firebase/FirebaseApi";
+import { HOMEPAGE_ROUTE } from "../AppConstants";
 
-export default function FoodInput() {
-    const [date, setDate] = useState(new Date());
-    const [dateText, setDateText] = useState('Select the expiry date') 
-    const [foodName, setFoodName] = useState('');
+export default function FoodInput({ navigation }) {
+  const [date, setDate] = useState<Date>(new Date());
+  const [dateText, setDateText] = useState("Select the expiry date");
+  const [foodName, setFoodName] = useState<string>("");
+  const [image, setImage] = useState("");
 
-    const onChange = (event:Event, selectedDate:Date) => {
-        const currentDate:Date = selectedDate || date;
-        setDate(currentDate);
+  const onChange = (event: Event, selectedDate: Date) => {
+    const currentDate: Date = selectedDate || date;
+    setDate(currentDate);
 
-        let tempDate = new Date(currentDate)
-        let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
-        setDateText('Expiry Date: ' + fDate)
-    }
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.textTitle}>Add a new Food Item</Text>
-            <View style={{marginTop:20, width:'100%', justifyContent:'center'}}>
-                <TextInput
-                  style={styles.input}
-                  onChangeText={setFoodName}
-                  value={foodName}
-                  placeholder="Name of the food Item"
-                />
-                <Text style={styles.fieldTitle}>{dateText}</Text> 
-
-                <DateTimePicker
-                    testID='datePicker'
-                    value={date}
-                    onChange={onChange}
-                    mode='date'
-                    display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-                />
-
-
-                <View style={styles.viewPaired}>
-                    <View style={styles.twoButtonContainer} > 
-                      <Button title="Take a Picture" onPress={() => captureImage()}/>
-                    </View>
-                    <View style={styles.twoButtonContainer} >
-                      <Button title="Upload a Picture" onPress = {() => uploadImage()}/>
-                    </View>
-                </View>
-            </View>
-        </View>
-    )
-
-}
-
-const captureImage = () => {
-    ImagePicker
-    .requestCameraPermissionsAsync()
-    .then(cameraRollPerm => {
-      // only if user allows permission to camera roll
-      if (cameraRollPerm.granted) {
-        ImagePicker
-        .launchCameraAsync({
-          allowsEditing: true,
-          aspect: [4, 3],
-        })
-        .catch(console.error)
-      } else {
-          Alert.alert('Provide access to camera and photos in settings');
-      }
-    })
-    .catch(console.error);
+    let tempDate = new Date(currentDate);
+    let fDate =
+      tempDate.getDate() +
+      "/" +
+      (tempDate.getMonth() + 1) +
+      "/" +
+      tempDate.getFullYear();
+    setDateText("Expiry Date: " + fDate);
   };
 
-const uploadImage = () => {
-  ImagePicker
-  .requestCameraPermissionsAsync()
-  .then(cameraRollPerm => {
-    // only if user allows permission to camera roll
-    if (cameraRollPerm.granted) {
-      ImagePicker
-      .launchImageLibraryAsync({
-        allowsEditing: true,
-        aspect: [4, 3],
+  const captureImage = () => {
+    ImagePicker.requestCameraPermissionsAsync()
+      .then(async (cameraRollPerm) => {
+        // only if user allows permission to camera roll
+        if (cameraRollPerm.granted) {
+          let result = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [4, 3],
+          }).catch(console.error);
+          if (!result?.cancelled && result.uri) {
+            setImage(result.uri);
+          }
+        } else {
+          Alert.alert("Provide access to camera and photos in settings");
+        }
       })
-      .catch(console.error)
-    } else {
-        Alert.alert('We need permission to go further!');
+      .catch(console.error);
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    }).catch(console.error);
+
+    if (result?.uri && !result?.cancelled) {
+      setImage(result.uri);
     }
-  })
-  .catch(console.error);
-};
+  };
+
+  const submit = async () => {
+    if (image !== "" && foodName !== "") {
+      let url = await uploadImage(image);
+      url = url ? url : "";
+      const uid = getCurrentUserUid();
+      if (uid && foodName !== "") {
+        addFoodItemToUser(uid, {
+          expiry: date,
+          name: foodName,
+          quantity: 1,
+          photoUrl: url,
+        });
+      }
+      navigation.navigate(HOMEPAGE_ROUTE)
+    } else {
+      Alert.alert("Fields Empty" + image + foodName);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>Add a new Food Item</Text>
+      <View style={{ marginTop: 20, width: "100%", justifyContent: "center" }}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setFoodName}
+          value={foodName}
+          placeholder="Name of the food Item"
+        />
+        <Text style={styles.fieldTitle}>{dateText}</Text>
+
+        <DateTimePicker
+          testID="datePicker"
+          value={date}
+          onChange={onChange}
+          minimumDate={new Date()}
+          mode="date"
+          display={Platform.OS === "ios" ? "spinner" : "default"}
+        />
+
+        <View style={styles.viewPaired}>
+          <View style={styles.twoButtonContainer}>
+            <Button title="Take a Picture" onPress={() => captureImage()} />
+          </View>
+          <View style={styles.twoButtonContainer}>
+            <Button title="Upload a Picture" onPress={() => pickImage()} />
+          </View>
+        </View>
+        <View style={{ width: 300, alignSelf: "center" }}>
+          <Button onPress={submit} title="Submit Details" />
+        </View>
+        <View style={{ width: 300, alignSelf: "center" }}>
+          <Button onPress={() => navigation.navigate(HOMEPAGE_ROUTE)} title="Back to Home" />
+        </View>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-    }, 
-    textTitle: {
-        fontFamily: 'Foundation',
-        fontSize: 35,
-        marginTop: 100,
-        alignSelf:'center'
-    },
-    fieldTitle: {
-        fontFamily: 'Foundation',
-        fontSize: 25,
-        marginTop: 20,
-        alignSelf:'center',
-        color:'red'
-    },
-    input: {
-        height: 50,
-        borderWidth: 1,
-        padding: 10,
-        width: '80%',
-        justifyContent:'center',
-        alignContent:'center',
-        alignSelf:'center'
-    },  
-    buttonContainer: {
-        backgroundColor: '#fff',
-        alignSelf: 'flex-end'
-    },
-    viewPaired: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    twoButtonContainer: {
-        flex: 1,
-    }, 
-    modalView: {
-        justifyContent: 'center',
-        minHeight:500
-    }
-    
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  textTitle: {
+    fontSize: 35,
+    marginTop: 100,
+    alignSelf: "center",
+  },
+  fieldTitle: {
+    fontSize: 25,
+    marginTop: 20,
+    alignSelf: "center",
+    color: "red",
+  },
+  input: {
+    height: 50,
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+    justifyContent: "center",
+    alignContent: "center",
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "#fff",
+    alignSelf: "flex-end",
+  },
+  viewPaired: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  twoButtonContainer: {
+    flex: 1,
+  },
+  modalView: {
+    justifyContent: "center",
+    minHeight: 500,
+  },
+});
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,54 +1,60 @@
-import { View, Text, SafeAreaView, FlatList } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, FlatList, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Greeter from '../components/greeter'
 import styles from '../styles/styles'
 import CustomInput from '../components/customInput'
 import Spacer from '../components/spacer'
 import Category from '../components/category'
-import Transaction from '../components/transaction'
+// import Transaction from '../components/transaction'
 import values from '../constants/values'
 import { categories, transactions } from '../constants/data'
+import { getName } from '../firebase/FirebaseApi'
 
-export default function HomeScreen()  {
-  return (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.horizontalPaddingView}>
-            <Greeter user={{
-                    img: 'https://media-exp1.licdn.com/dms/image/C5603AQGYfxv0fbIobw/profile-displayphoto-shrink_400_400/0/1626720808052?e=1662595200&v=beta&t=jL-Eak3EyvfYlAxveh3kClt-KKVxI8AKq6iUNxabPDs',
-                    name: 'Mrinal'
-                    }}
-                    />
-            <Spacer height={20}/>
-            <CustomInput placeholder='Search' icon='search-outline' />
-            <Spacer height={20} />
-            <Text style={values.h2Style}>Categories of food</Text>
-            <Spacer height={20} />
+export default function HomeScreen({ navigation })  {
+    const [name, setName] = useState<string>('')
+    useEffect(() => {
+        setName(getName())
+    })
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.horizontalPaddingView}>
+                <Greeter user={{
+                        img: 'https://media-exp1.licdn.com/dms/image/C5603AQGYfxv0fbIobw/profile-displayphoto-shrink_400_400/0/1626720808052?e=1662595200&v=beta&t=jL-Eak3EyvfYlAxveh3kClt-KKVxI8AKq6iUNxabPDs',
+                        name: name
+                        }}
+                        />
+                <Spacer height={20}/>
+                <CustomInput placeholder='Search' icon='search-outline' />
+                <Spacer height={20} />
+                <Text style={values.h2Style}>Categories of food</Text>
+                <Spacer height={20} />
 
-        </View>
-        <View style={{paddingLeft: values.horizontalPadding}}>
-            <FlatList
-                horizontal
-                data={categories}
-                showsHorizontalScrollIndicator={false}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => <Category category={item} onPress={(val) => console.warn(`Clicked ${val}`)} />}
-            />
-        </View>
+            </View>
+            <View style={{paddingLeft: values.horizontalPadding}}>
+                <Button title="Add Food Item" onPress={navigation.navigate("Foodinput")}/>
+                <FlatList
+                    horizontal
+                    data={categories}
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => <Category category={item} onPress={(val) => console.warn(`Clicked ${val}`)} />}
+                />
+            </View>
 
-        <View style={styles.horizontalPaddingView}>
-            <Spacer height={20} />
-            <Text style={values.h2Style}>Recipes</Text>
-            <Spacer height={20} />
-            <FlatList
-                
-                data={transactions}
-                showsVerticalScrollIndicator={false}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => <Transaction transaction={item} onPress={(val) => console.warn(`Clicked ${val}`)} />}
-            />
-        </View>
-      
-    </SafeAreaView>
-  )
+            <View style={styles.horizontalPaddingView}>
+                <Spacer height={20} />
+                <Text style={values.h2Style}>Recipes</Text>
+                <Spacer height={20} />
+                {/* <FlatList
+                    
+                    data={transactions}
+                    showsVerticalScrollIndicator={false}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => <Transaction transaction={item} onPress={(val) => console.warn(`Clicked ${val}`)} />}
+                /> */}
+            </View>
+        
+        </SafeAreaView>
+    )
 }
 
